563401f790bb008d54313bd6f2c661e5c94651b2
Charumathi Badrinath <charumathi.badrinath@gmail.com>
2022-09-16 14:17:14 -0400

    changes

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "04a0d365a069df6796259842abcf05e0",
-"index.html": "fc06415ab22520c1acdeaa659841f704",
-"/": "fc06415ab22520c1acdeaa659841f704",
+"index.html": "4d4b0918c4ce0d4a5096f32b43a648d2",
+"/": "4d4b0918c4ce0d4a5096f32b43a648d2",
 "main.dart.js": "f1ae28c188ae514593dd4e082355cfad",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
